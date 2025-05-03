e1fb16ae41f79f3fc521b5c829a20e014420df45
bestolochina <bestolochina@gmail.com>
2025-05-03 18:53:05 +0700

    Initial commit

---
+++ NBA Data Preprocessing/task/preprocess.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+import requests
+
+# Checking ../Data directory presence
+if not os.path.exists('../Data'):
+    os.mkdir('../Data')
+
+# Download data if it is unavailable.
+if 'nba2k-full.csv' not in os.listdir('../Data'):
+    print('Train dataset loading.')
+    url = "https://www.dropbox.com/s/wmgqf23ugn9sr3b/nba2k-full.csv?dl=1"
+    r = requests.get(url, allow_redirects=True)
+    open('../Data/nba2k-full.csv', 'wb').write(r.content)
+    print('Loaded.')
+
+data_path = "../Data/nba2k-full.csv"
+
+# write your code here
